2157187e6e70989e1177b33a48dcf0157734be9d
Tejal Gaikwad <you@example.com>
2022-09-10 18:48:35 +0530

    Code to calculate wage for 100 working hrs

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,33 +1,35 @@
 package com.bridelabz;
 
 public class EmployeeWage {
-
 	public static final int Emp_present = 1;
 	public static final int Half_time = 2;
 	public static final int Wage_per_hour = 20;
 	public static final int Num_of_working_days = 20;
+	public static final int Max_hours_in_month = 100;
 
 	public static void main(String[] args) {
 
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
-		for (int days = 0; days < Num_of_working_days; days++) {
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= Max_hours_in_month && totalWorkingDays < Num_of_working_days) {
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 			switch (empCheck) {
 			case Emp_present:
-				empHrs = 4;
+				empHrs = 8;
 				break;
 			case Half_time:
-				empHrs = 8;
+				empHrs = 4;
 				break;
 			default:
 				empHrs = 0;
 			}
-
-			empWage = empHrs * Wage_per_hour;
-			totalEmpWage += empWage;
-			System.out.println("Total_Salary :" + empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Days : " + totalWorkingDays + " Emp Hrs: " + empHrs);
 		}
+		int totalEmpWage = totalEmpHrs * Wage_per_hour;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 
